537b4824d234997591ef2fa14e50eeb297255acd
Tommy Bryan <tommybryan0713@gmail.com>
2024-10-22 11:06:05 -0400

    changed file 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," 
+ * main - prints "with proper grammar, but the outcome is a piece of art,\n" 
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
