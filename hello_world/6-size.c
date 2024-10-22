d0031ac95776f2f42c2a29b41ea813a73aba42a2
Tommy Bryan <tommybryan0713@gmail.com>
2024-10-22 13:46:27 -0400

    Changed file 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 	printf("Size of char: %zu byte\n", sizeof(char));
 	printf("Size of int: %zu byte\n", sizeof(int));
 	printf("Size of long int: %zu byte\n", sizeof(long int));
-	printf("Size of long long int: %zu byte\n", sizeof(long long int));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of float: %zu byte\n", sizeof(float));
 
 	return (0);
