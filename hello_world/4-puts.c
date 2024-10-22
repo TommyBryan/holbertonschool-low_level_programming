a95034b83c2f2859d9688c06283b1ba88b854c0b
Tommy Bryan <tommybryan0713@gmail.com>
2024-10-22 10:33:00 -0400

    Changed file 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,6 @@
-/* Tommy Hernandez Arroyo */
-
 #include <stdio.h>
-
-/** Main - prints "\"Programming is like building a multilingual puzzle", followed by a new line
- *  Return: Always 0.
- *
- */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
