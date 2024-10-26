5c1a95d26f58a3c6ef023aff124c32b44fd504fa
Tommy Bryan <tommybryan0713@gmail.com>
2024-10-26 13:55:16 -0400

    Made changes in the C code of the file 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,12 +10,12 @@ int main(void)
 
 	for (number = '0'; number <= '9'; number++)
 	{
-		putchar(number);
-		if (number != 9)
+		if (number != 10)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+		putchar(number);
 	}
 	putchar('\n');
 
