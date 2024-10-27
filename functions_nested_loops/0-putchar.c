acef6c175f759dec2e9481d4ce6d25acdaf517c5
Tommy Bryan <tommybryan0713@gmail.com>
2024-10-27 00:55:20 -0400

    Changed commands in 0-putchar.c, modified main.h and added file _putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - prints _putchar followed by a new line
- *
+ * main - Using _putchar to print "_putchar" followed by a new line
+ * @c: print "_putchar" and add a new line
  * Return: Always 0
  */
+int _putchar(char c);
+
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
