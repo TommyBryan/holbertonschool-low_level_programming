026b088fccc98ad50f2ad5a34cde23ab24de99ba
Tommy Bryan <tommybryan0713@gmail.com>
2024-10-27 11:19:52 -0400

    Added file 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lower case,
+ * followed by a new line.
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
