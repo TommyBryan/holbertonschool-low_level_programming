1123565a8097f03f4422a3a7c966cc4108cd7978
Tommy Bryan <tommybryan0713@gmail.com>
2024-10-27 23:01:18 -0400

    Added file 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isalpha - Returns 1 if lower or upper case 
+ *@c - checks if c is lowercase or uppercase
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
