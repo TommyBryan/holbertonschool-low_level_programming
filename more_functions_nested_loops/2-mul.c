b0dfd57af1e36ad3533efe68147f47744af9af6f
Tommy Bryan <tommybryan0713@gmail.com>
2024-10-29 10:57:56 -0400

    Added file 2-mul.c and 2-main.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplies two intergers.
+ *
+ * @a: stores value
+ *
+ * @b: stores value
+ *
+ * Return: a , b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
