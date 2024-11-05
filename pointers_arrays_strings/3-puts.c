9c4d24469c04dd05d018aa6fc9042a8cb4789a16
Tommy Bryan <tommybryan0713@gmail.com>
2024-11-04 22:23:41 -0400

    Made changes in file 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _puts - prints a string, followed by a new line
- * @str: s
+ * _puts - prints a string, followed by a new line to stdout
+ * @str: string to print
+ *
+ * Return: void
  */
+
 void _puts(char *str)
 {
 	int i;
 
 	for (i = 0; str[1] != '\0'; i++)
-		_puts(str[i]);
-	_puts('\n');
+		_putchar(str[i]);
+	_putchar('\n');
 }
