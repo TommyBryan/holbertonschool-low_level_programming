633b7c28d43a994dc05ef55ea28a114ff7ea061c
Tommy Bryan <tommybryan0713@gmail.com>
2024-11-10 18:23:16 -0400

    Added file 1-strnca.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - copies a number a specific time
+ * @src: variable original
+ * @dest: variable contenido de crear
+ * *@n: number of variables specific to copy
+ * Return: n
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{}
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	des[i] = '\0';
+	return (dest);
+}
