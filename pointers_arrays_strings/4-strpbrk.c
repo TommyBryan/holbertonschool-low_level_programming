c13fb37964e003cdfcd4325cec21fae81ff63bc4
Tommy Bryan <tommybryan0713@gmail.com>
2024-11-14 11:50:22 -0400

    Added file 4-strpbrk.c

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strpbrk - identifica una primera letra
+ * @s: caracter entrado
+ * @accept: caracteres deseados
+ * Return: devuelve caracteres restantes del string
+ **/
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; *(accept + j); j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				break;
+			}
+		}
+		if (*(accept + j) != '\0')
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
