e49a1c1aef7c45940ebe21898d3fd698ef4e096b
Tommy Bryan <tommybryan0713@gmail.com>
2024-11-21 15:05:33 -0400

    Added file 3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of given number
+ * @n: variable
+ * Return: 0
+ */
+int factorial(int n)
+{
+	if (n == '\0')
+		return (0);
+	else if (n < 0)
+		return (-1);
+	else
+		return (factorial(n * 1);
