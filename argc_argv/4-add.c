3982d0dec6b13442f9ab01d381919fbb87a3aa14
TommyBryan <tommy0713@gmail.com>
2024-11-24 12:24:04 -0400

    Added files

---
+++ argc_argv/4-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -
+ *
+ * @argc: arguments
+ * @argv: String of arguments
+ *
+ * Return: 0 (Succesful) or 1 (Error)
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else if
