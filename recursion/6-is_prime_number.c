43d266576e5eb80947ca8e102a5650ab650b123e
TommyBryan <tommy0713@gmail.com>
2024-11-23 22:46:26 -0400

    Made changes to the script of file 6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,31 @@
+
 #include "main.h"
 /**
  * is_prime_helper - divides number
+ *@n: number to check
+ *@div: divisor to test
  *
- *
- * Return: divided number
+ * Return: 0 if diversible, 1 otherwise.
+ */
+int is_prime_helper(int n, int div)
+{
+	if (div == 1)
+		return (1); /* No divisor found, n is prime */
+	if (n % div == 0)
+		return (0); /* Found a divisor, n is not prime */
+	return (is_prime_helper(n, div - 1)); /*check next divisor*/
+}
+
+/**
+ * is_prime_number - checks if number is a prime number
+ * @n: Number to check
+ * Return: 1 if n is a prime number, if not return 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0); /* Number less or equal to 1 are not Prime */
+	return (is_prime_helper(n, n - 1)); /* Start checking divisors */
+}
+
+
