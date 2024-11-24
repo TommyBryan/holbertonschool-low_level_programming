50d255907a80bc82fd52a4004d6025c315c376a5
TommyBryan <tommybryan0713@gmail.com>
2024-11-24 03:15:23 -0400

    Added file 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the programs name
+ *
+ * @argc: Argument count (unused)
+ * @argv: Argument vector (array of strings)
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
