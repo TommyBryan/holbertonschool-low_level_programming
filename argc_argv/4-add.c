c1e08bc1af4bb594f69891d3b26bd15418e29e29
TommyBryan <tommy0713@gmail.com>
2024-11-25 11:23:48 -0400

    Made changes in file 4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,19 +2,37 @@
 #include <stdlib.h>
 
 /**
- * main -
+ * main - Add positive numbers passed as arguments
  *
- * @argc: arguments
- * @argv: String of arguments
+ * @argc: Number of arguments
+ * @argv: Array of arguments strings
  *
  * Return: 0 (Succesful) or 1 (Error)
  */
 
 int main(int argc, char **argv)
 {
+	int sum = 0, i, j;
+
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	else if
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
