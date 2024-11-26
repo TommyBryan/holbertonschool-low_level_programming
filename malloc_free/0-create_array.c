340b5ba220e2e60afe96d90518e7e2a603d6bc69
TommyBryan <tommybryan0713@gmail.com>
2024-11-25 21:30:43 -0400

    Made changes

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,51 +1,26 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
+ * create_array - crea un arreglo con la misma letra
+ * @size: tamano deseado de espacio
+ * @c: letra a colocar en el array
+ * Return: return 0
+ **/
+
+char *create_array(unsigned int size, char c)
 {
+	char *ac;
 	unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
-}
+	if (size == 0)
+		return (NULL);
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char *buffer;
+	ac = malloc(size * sizeof(*ac));
+	if (ac == NULL)
+		return (NULL);
 
-	buffer = create_array(98, 'H');
-	if  (buffer == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	simple_print_buffer(buffer, 98);
-	free(buffer);
-	return (0);
+	for (i = 0; i <= size; i++)
+		ac[i] = c;
+	return (ac);
 }
