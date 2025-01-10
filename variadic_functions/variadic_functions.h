380e4f3dcc7b9a4b073d499eb5875bc5f4640c58
TommyBryan <tommy0713@gmail.com>
2025-01-10 11:16:07 -0400

    Made change

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,7 +3,7 @@
 void _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *sepatrator,const unsigned int n, ...);
+void print_strings(const char *sepatrator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 #endif
